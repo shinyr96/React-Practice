5f8af2a8f9552e44dfccd3b12de0d0f9489ae187
shinyr96 <yuli101033@hanmail.net>
2020-01-06 18:26:19 +0900

    Implement Customer Add Module & Customer List Refresh Module

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Customer from './components/Customer'
+import CustomerAdd from './components/CustomerAdd'
 import './App.css';
 import Paper from '@material-ui/core/Paper';
 import Table from '@material-ui/core/Table';
@@ -36,9 +37,22 @@ props or state => shouldComponentUpdate()
 
 class App extends Component{
 
-state = {
-  customers : "",
-  completed : 0
+constructor(props){
+  super(props);
+  this.state = {
+    customers : '',
+    completed : 0
+  }
+}
+
+stateRefresh = () => {
+  this.setState({
+    customers:'',
+    completed : 0
+  });
+  this.callApi()
+  .then(res => this.setState({customers: res}))
+  .catch(err => console.log(err));
 }
 
 componentDidMount() {
@@ -62,6 +76,7 @@ progress = () => {
   render(){
     const {classes}=this.props;
     return (
+      <div>
       <Paper className={classes.root}>
         <Table className={classes.table}>
           <TableHead>
@@ -98,6 +113,8 @@ progress = () => {
           </TableBody>
         </Table>
       </Paper>
+      <CustomerAdd stateRefresh={this.stateRefresh} />
+      </div>
     );
   }
 }
